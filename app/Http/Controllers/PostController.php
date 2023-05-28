fb062db4de1700bc89d43b29f4584f7fe8b0b52c
piy0hina <p0y0n.hina@gmail.com>
2023-05-28 17:01:24 +0900

    記事作成画面 入力チェック処理(バリデーション)

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,29 +2,34 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use App\Models\Post;
+use App\Http\Requests\PostRequest;
 
 class PostController extends Controller
 {
+    
     public function index(Post $post)
     {
         //limit_countでpaginateされたメソッドの呼び出し
         return view('posts/index')->with(['posts' => $post->getPaginateByLimit()]);
     }
+    
     public function show(Post $post)
     {
         //任意idのpostインスタンスをpost変数に格納
         return view('posts.show')->with(['post' => $post]);
     }
+    
     public function create()
     {
         return view('posts/create');
     }
-    public function store(Request $request, Post $post)
+    
+    public function store(Post $post, PostRequest $request)
     {
         $input = $request['post'];
         $post->fill($input)->save();
         return redirect('/posts/' . $post->id);
     }
+    
 }
--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -15,12 +15,14 @@
             @csrf
             <div class="title">
                 <h2>タイトルを入力してください</h2>
-                <input type="text" name="post[title]" placeholder="タイトル" />
+                <input type="text" name="post[title]" placeholder="タイトル" value="{{ old('post.title') }}"/>
+                <p class="title_error" style="color:red">{{ $errors->first('post.title') }}</p>
             </div>
             
             <div class="body">
                 <h2>本文を入力してください</h2>
-                <textarea name="post[body]" placeholder="本文"></textarea>
+                <textarea name="post[body]" placeholder="本文" value="{{ old('post.body') }}"></textarea>
+                <p class="body_error" style="color:red">{{ $errors->first('post.body') }}</p>
             </div>
             
             <input type="submit" value="保存" />
